8147ea04e18c6e3085b061f25c7fc89e402343c1
Andrew <drw026@gmail.com>
2020-03-07 18:08:08 +0100

    add javascript entrypoint for modules

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,3 +1,15 @@
-import hello from "./popup/example";
+import { getCurrentTabData, getSegment } from './popup/browserDataService';
+import info from './popup/info';
+import environment from './popup/environment';
+import setCookie from './popup/setCookie';
+import openFeature from './popup/openFeature';
 
-hello();
+(async () => {
+    const currentTabData = await getCurrentTabData();
+    const currentSegment = await getSegment();
+    
+    info(document.querySelector('.info'), { currentTabData, currentSegment });
+    environment(document.querySelector('.environment'), { currentTabData });
+    setCookie(document.querySelector('.set-cookie'));
+    openFeature(document.querySelector('.open-feature'));    
+})();
