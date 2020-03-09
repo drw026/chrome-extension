bad79c8253c2b72b1754c0527e1506f5cfd16520
Andrew Reasoa <a.reasoa@beslist.nl>
2020-03-09 10:58:44 +0100

    added header and footer with logos

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,6 +1,8 @@
 import '../img/beslist16.png';
 import '../img/beslist48.png';
 import '../img/beslist128.png';
+import '../img/logo-tweb.png';
+import '../img/logo-beslist.png';
 import { getCurrentTabData, getSegment } from './popup/browserDataService';
 import info from './popup/info';
 import environment from './popup/environment';
@@ -10,9 +12,9 @@ import openFeature from './popup/openFeature';
 (async () => {
     const currentTabData = await getCurrentTabData();
     const currentSegment = await getSegment();
-    
+
     info(document.querySelector('.info'), { currentTabData, currentSegment });
     environment(document.querySelector('.environment'), { currentTabData });
     setCookie(document.querySelector('.set-cookie'));
-    openFeature(document.querySelector('.open-feature'));    
+    openFeature(document.querySelector('.open-feature'));
 })();
